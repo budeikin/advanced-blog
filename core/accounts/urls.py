4d7d6c5e9020ef81d5bf5bfaabf18e453f437f4a
budeikin <budeikin52@gmail.com>
2023-07-08 17:19:10 +0330

    'ready_to_staging'

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import send_email
+from .views import send_email,test
 
 app_name = "accounts"
 
 urlpatterns = [
     path('send/',send_email),
+    path('test/',test,name='test'),
     path("api/v1/", include("accounts.api.v1.urls")),
     path("", include("django.contrib.auth.urls")),
 ]
--- core/accounts/views.py
+++ core/accounts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from .tasks import sendMail
-from django.http import HttpResponse 
+from django.http import HttpResponse,JsonResponse
+import requests
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 # Create your views here.
 
 def send_email(request):
     sendMail.delay()
     return HttpResponse('sent sucessfully')
+
+@cache_page(60)
+def test(request):
+    response = requests.get('https://ce001c70-2433-4dd9-97d8-49c3c83af911.mock.pstmn.io/test/delay/5/')
+    return JsonResponse(response.json())
